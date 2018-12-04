a231bf4288b16d01e9124a201b3e89b30829f9a7
bill.mark <2286449127@qq.com>
2018-12-04 17:08:34 +0800

    多模块加传参

---
+++ 多模块例子/src/components/a.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="vuex">
+		<div>page  a {{$store.state.money.money}}</div>
+		<button type="button" name="button" @click="add">增加</button>
+		<button type="button" name="button" @click="reduce">删减</button>
+	</div>  
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+export default {
+	methods:{
+		...mapActions('money',['add','reduce'])
+	}
+}
+</script>
+
+<style lang="css">
+	
+</style>
---
+++ 多模块例子/src/components/b.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="vuex">
+		<div>page b {{$store.state.count.count}}</div>
+		<button type="button" name="button" @click="add(2)">增加</button>
+		<button type="button" name="button" @click="reduce">删减</button>
+	</div>  
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+export default {
+	methods:{
+		...mapActions('count',['add','reduce'])
+	}
+	
+}
+</script>
+
+<style lang="css">
+	
+</style>
